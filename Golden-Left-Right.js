42e5c800e613de7694377006096f2c470ade91fc
Suysker <120855524@qq.com>
2024-09-07 17:33:15 +0800

    Revert "update readme"
    This reverts commit 43e647961b4b095273a249575ac6d0c25118ee6b.

--- Golden-Left-Right.js
+++ Golden-Left-Right.js
@@ -2,7 +2,7 @@
 // @name         黄金左右键
 // @description  按住"→"键倍速播放, 按住"←"键减速播放, 松开恢复原来的倍速，灵活追剧看视频~ 支持用户单独配置倍速和秒数，并可根据根域名启用或禁用脚本
 // @icon         https://image.suysker.xyz/i/2023/10/09/artworks-QOnSW1HR08BDMoe9-GJTeew-t500x500.webp
-// @namespace    https://blog.suysker.xyz/
+// @namespace    http://tampermonkey.net/
 // @version      1.0.0
 // @author       Suysker
 // @match        http://*/*
